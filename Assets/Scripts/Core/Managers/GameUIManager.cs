5ceece141611d5c05cd9cdb53279407660f39b71
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-09-02 16:39:16 +0800

    game over screen added

--- Assets/Scripts/Core/Managers/GameUIManager.cs
+++ Assets/Scripts/Core/Managers/GameUIManager.cs
@@ -4,20 +4,37 @@ using TMPro;
 
 public class GameUIManager : MonoBehaviour
 {
+    [Header("In Game Display")]
     [SerializeField] private GameObject playerCanvas;
     [SerializeField] private TextMeshProUGUI playerLivesCount;
     [SerializeField] private TextMeshProUGUI timerCountDisplay;
+    
+    [Header("End Game Display")]
+    [SerializeField] private GameObject GameEndCanvas;
+    [SerializeField] private TextMeshProUGUI GameEndText;
+    
     private PlayerCombat _playerCombat;
-
 
     private void OnEnable()
     {
-        GameManager.Instance.OnGameStart += () => playerCanvas.SetActive(true);
+        GameManager.Instance.OnGameStart += () =>
+        {
+            playerCanvas.SetActive(true);
+            GameEndCanvas.SetActive(false);
+        };
+        
+        GameManager.Instance.OnGameEnd += () =>
+        {
+            playerCanvas.SetActive(false);
+            GameEndCanvas.SetActive(true);
+            GameOver();
+        };
     }
     
     private void Start()
     {
         playerCanvas.SetActive(false);
+        GameEndCanvas.SetActive(false);
     }
 
     private void Update()
@@ -28,4 +45,19 @@ public class GameUIManager : MonoBehaviour
         var timespan = TimeSpan.FromSeconds(GameManager.Instance.Timer);
         timerCountDisplay.text = $"{(int) timespan.TotalMinutes}:{timespan.Seconds:00}";
     }
+
+    private void GameOver()
+    {
+        if (GameManager.Instance.playerWon)
+        {
+            GameEndText.text = "Congratulations";
+            GameEndText.color = Color.yellow;
+        }
+        else
+        {
+            GameEndText.text = "Game Over";
+            GameEndText.color = Color.red;
+        }
+    }
+    
 }
