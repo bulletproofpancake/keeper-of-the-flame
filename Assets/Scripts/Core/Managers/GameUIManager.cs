914e11fd977921e4918881a1ec1efdf9bb36fa0e
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-09-02 10:54:14 +0800

    player canvas now loads on game start

--- Assets/Scripts/Core/Managers/GameUIManager.cs
+++ Assets/Scripts/Core/Managers/GameUIManager.cs
@@ -3,13 +3,25 @@ using TMPro;
 
 public class GameUIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject playerCanvas;
     [SerializeField] private TextMeshProUGUI playerLivesCount;
     
     private PlayerCombat _playerCombat;
 
+
+    private void OnEnable()
+    {
+        GameManager.Instance.OnGameStart += () => playerCanvas.SetActive(true);
+    }
+
     private void Awake()
     {
         _playerCombat = FindObjectOfType<PlayerCombat>();
+    }
+
+    private void Start()
+    {
+        playerCanvas.SetActive(false);
     }
 
     private void Update()
